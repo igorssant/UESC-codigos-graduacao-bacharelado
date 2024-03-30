997d40c2001498b7aec9ce9bba567ca972a8cc27
igorssant <31260344+igorssant@users.noreply.github.com>
2024-03-30 12:21:58 -0300

    update: criando novos arquivos & novo mapaTokens

---
+++ compiladores/parte-2/AnalizadorLexico/src/com/analizadorSintatico/tokens/Tokens.java
@@ -0,0 +1,36 @@
+package com.analizadorSintatico.tokens;
+
+import java.util.HashMap;
+
+public class Tokens {
+    public static final HashMap<String, String> mapaToken = new HashMap<>();
+
+    static {
+        /* TIPOS NUMERICOS */
+        mapaToken.put();
+        mapaToken.put();
+
+        /* TIPO ENDERECO */
+        mapaToken.put();
+
+        /* TIPO ALFANUMERICO */
+        mapaToken.put();
+
+        /* TIPOS COMENTARIOS */
+        mapaToken.put();
+        mapaToken.put();
+        mapaToken.put();
+
+        /* TIPOS DE SINAIS MATEMETICOS */
+        mapaToken.put();
+        mapaToken.put();
+
+        /* TIPOS DE SINAIS LOGICOS */
+
+        /* TIPO ATRIBUICAO */
+
+        /* TIPOS DE TERMINACAO */
+
+        /* TIPOS DE USO GERAL */
+    }
+}
