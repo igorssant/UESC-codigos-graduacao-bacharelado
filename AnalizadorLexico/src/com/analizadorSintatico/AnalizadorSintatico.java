eea93bc7bcc701e91890baad8ffdad5148c4d5c4
Igor Sousa dos Santos Santana <igorssant@hotmail.com>
2024-03-30 11:39:39 -0300

    update: criação dos primeiros arquivos

---
+++ AnalizadorLexico/src/com/analizadorSintatico/AnalizadorSintatico.java
@@ -0,0 +1,7 @@
+package com.analizadorSintatico;
+
+public class AnalizadorSintatico {
+    public static void main(String[] args) {
+        System.out.println("working");
+    }
+}
---
+++ AnalizadorLexico/src/com/analizadorSintatico/automato/Automato.java
@@ -0,0 +1,4 @@
+package com.analizadorSintatico.automato;
+
+public class Automato {
+}
