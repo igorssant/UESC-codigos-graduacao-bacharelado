09c276eb193eabd633994c4082cc9c6c4b61de90
igorssant <31260344+igorssant@users.noreply.github.com>
2024-08-17 13:12:55 -0300

    Delete notebooktcc directory

--- notebooktcc/src/utils/pegarsubstring.py
+++
@@ -1,9 +0,0 @@
-def substring(frase:str, separacao:str= "= ") -> str:
-     try:
-          frase = frase.split(separacao, 1)[1]
-          frase = frase.split("\n")[0]
-     
-     except IndexError:
-          frase = ""
-          
-     return frase
